a14858ee14b7348f078ce290ac79945b74a35e9e
arkadiusz-wiatr <49699345+arkadiusz-wiatr@users.noreply.github.com>
2020-04-02 12:05:18 +0200

    LibGDX
    Add libGDX to the project.

---
+++ android/src/com/awiatr/fred/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.awiatr.fred;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.awiatr.fred.Fred;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Fred(), config);
+	}
+}
---
+++ desktop/src/com/awiatr/fred/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.awiatr.fred.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.awiatr.fred.Fred;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fred(), config);
+	}
+}
