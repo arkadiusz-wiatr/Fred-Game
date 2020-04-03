be39977404ab97c760b788ddcac15d36356cd1cd
arkadiusz-wiatr <49699345+arkadiusz-wiatr@users.noreply.github.com>
2020-04-04 00:30:55 +0200

    Creating HUD and game labels.

---
+++ core/src/com/awiatr/fred/Scenes/Hud.java
@@ -0,0 +1,62 @@
+package com.awiatr.fred.Scenes;
+
+import com.awiatr.fred.Fred;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer time;
+    private Integer score;
+    private Integer lives;
+
+    Label timeWordLabel;
+    Label livesWordLabel;
+    Label scoreWordLabel;
+    Label scoreLabel;
+    Label livesLabel;
+    Label timeLabel;
+
+
+    public Hud(SpriteBatch sb){
+        time = 0;
+        score = 0;
+        lives = 3;
+
+        viewport = new FitViewport(Fred.V_WIDTH, Fred.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport,sb);
+
+        Table table = new Table();
+        table.bottom();
+        table.setFillParent(true);
+
+        timeWordLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        livesWordLabel = new Label("LIVES", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreWordLabel =  new Label("SCORE", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel =  new Label(String.format("%06d",score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        livesLabel  = new Label(String.format("%03d", lives), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label(String.format("%06d", time), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+
+        table.add(scoreWordLabel).expandX().padTop(10);
+        table.add(timeWordLabel).expandX().padTop(10);
+        table.add(livesWordLabel).expandX().padTop(10);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(timeLabel).expandX();
+        table.add(livesLabel).expandX();
+
+        stage.addActor(table);
+    }
+
+}
---
+++ core/src/com/awiatr/fred/Screens/PlayScreen.java
@@ -0,0 +1,71 @@
+package com.awiatr.fred.Screens;
+
+import com.awiatr.fred.Fred;
+import com.awiatr.fred.Scenes.Hud;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import javax.xml.soap.Text;
+
+public class PlayScreen implements Screen {
+    private Fred game;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+
+
+    public PlayScreen(Fred game){
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(Fred.V_WIDTH,Fred.V_HEIGHT,gamecam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+    }
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
